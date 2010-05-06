84a7127a78165319faf98340f56d273e0a59cf7a
vivion <vi@google.com>
2010-05-06 21:08:05 +0800

    markdown view

---
+++ md.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ "$#" != "1" ]; then
+  echo -e "Usage of the script:\t$0 inputFileName\n"
+  exit 1
+fi
+inputfile=$1
+outputfile="$1"".html"
+echo $inputfile
+echo $outputfile
+markdown $inputfile > $outputfile
+firefox $outputfile
